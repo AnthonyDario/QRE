db21b7fa9b731e2f71f597a5e7594a3fe7195217
Anthony Dario <adario@uoregon.edu>
2022-12-20 16:26:21 -0800

    Implement running CRAs

--- main.hs
+++ main.hs
@@ -1,6 +1,6 @@
 import Control.Exception (Exception, throw)
 import Data.Map ((!))
-import qualified Data.Map (Map, fromList, adjust, lookup)
+import qualified Data.Map (Map, toList, fromList, adjust, lookup)
 
 {-
  -  a CRA is a tuple A = (Q, X, Delta, I, F)
@@ -15,18 +15,23 @@ data CRAException = InvalidState deriving Show
 
 instance Exception CRAException
 
+{-
+ - The CRA type    
+ ------------------------------------
+ -}
+
 type State      = String
 type Register   = String
 type Registers  = Data.Map.Map Register Int
-type RegUpdate  = Int -> Registers -> Registers
-type Tag        = Char
+type Tag        = (Char, Int)
+type RegUpdate  = Tag -> Registers ->  Registers
 data Expression = Const Int | Var String | Op 
 
 type Q     = [State]
-type X     = Data.Map.Map Register Int
-type Delta = Data.Map.Map (State, Tag) (RegUpdate, State)
+type X     = Registers
+type Delta = Data.Map.Map (State, Char) (RegUpdate, State)
 type I     = State -> Registers
-type F     = State -> Registers -> Int
+type F     = State -> Registers -> Maybe Int
 
 type CRA = (Q, X, Delta, I, F)
 
@@ -34,21 +39,41 @@ instance Show F where show _ = "F"
 instance Show I where show _ = "I"
 instance Show RegUpdate where show _ = "RegUpdate"
 
-{-
-thetaA :: RegUpdate
-thetaA v rs = (Data.Map.adjust (\x -> x + v) "x" rs)
+{- CRA Helpers -}
 
-thetaB :: RegUpdate
-thetaB v rs = (Data.Map.adjust (\y -> y + v) "y" rs)
+-- Call the finalization function on a state
+output :: CRA -> State -> Registers -> Maybe Int
+output (_, _, _, _, f) s rs = f s rs
+
+-- Update the CRA to a new state and registers
+update :: CRA -> State -> Registers -> (Char, Int) -> (State, Registers)
+update (_, _, d, _, _) s rs e@(tag, _) = case d ! (s, tag) of 
+                                              (theta, s') -> (s', theta e rs)
+
+{-
+    Running a CRA:
+
+    Outer function initializes the CRA to a state then calls 
+    the inner function.
+
+    The inner function will execute the state machine a character at a time
 -}
+
+run :: CRA -> State -> [Tag] -> Maybe Int
+run cra@(_, _, _, i, _) s w = runInternal cra s (i s) w
+
+runInternal :: CRA -> State -> Registers -> [Tag] -> Maybe Int
+runInternal cra s rs []     = output cra s rs
+runInternal cra s rs (c:cs) = case update cra s rs c of 
+                                   (s', rs') -> runInternal cra s' rs' cs
 
 main = do
     print (let 
         states = ["p", "qa", "qb"]
         tags   = ['a', 'b']
         theta  = Data.Map.fromList [("x", 0), ("y", 0)] :: Registers
-        thetaA = (\v rs -> (Data.Map.adjust (\x -> x + v) "x" rs)) :: RegUpdate
-        thetaB = (\v rs -> (Data.Map.adjust (\y -> y + v) "x" rs)) :: RegUpdate
+        thetaA = (\(t, v) rs -> (Data.Map.adjust (\x -> x + v) "x" rs)) :: RegUpdate
+        thetaB = (\(t, v) rs -> (Data.Map.adjust (\y -> y + v) "y" rs)) :: RegUpdate
         delta  = Data.Map.fromList [(("p",  'a'), (thetaA, "qa")),
                                     (("p",  'b'), (thetaB, "qb")),
                                     (("qa", 'a'), (thetaA, "qa")),
@@ -59,8 +84,9 @@ main = do
                              "p" -> theta
                              _   -> throw InvalidState)
         final  = (\s rs -> case s of
-                                "qa" -> rs ! "x"
-                                "qb" -> rs ! "y") :: F
+                                "qa" -> Just (rs ! "x")
+                                "qb" -> Just (rs ! "y")
+                                _    -> Nothing)
         cra = (states, init "p", delta, init, final) :: CRA
         in
-            cra)
+            run cra "p" [('a', 4), ('a', 5), ('b', 10), ('a', 3), ('b', 4)])
